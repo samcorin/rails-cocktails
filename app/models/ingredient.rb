632676bc684e3397d1e5df93812c496db62f66ab
Sam Corin <samcorin@gmail.com>
2017-04-07 18:07:49 +0900

    modified seed.rb

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,4 @@
 class Ingredient < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :doses
-
-  searchkick
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 require 'open-uri'
 require 'json'
 
+# Dose.destroy_all
+# Cocktail.destroy_all
 Ingredient.destroy_all
 puts "Adding Ingredients"
 
